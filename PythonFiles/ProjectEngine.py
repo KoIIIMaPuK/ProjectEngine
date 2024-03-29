ae6a532978f6f400faadad7502e529b4f7241fe3
Undefined <Names2007@mail.ru>
2024-03-29 19:20:14 +0300

    update ProjectEngine.py: all information on promotions is collected

--- PythonFiles/ProjectEngine.py
+++ PythonFiles/ProjectEngine.py
@@ -1,24 +1,36 @@
 import asyncio
 import aiohttp
 import sys
-
 from bs4 import BeautifulSoup as BS
-from fake_useragent import UserAgent
 from datetime import datetime
+from fake_useragent import UserAgent
 
-BASE_URL = "https://ru.investing.com/equities/sberbank_rts"
+BASE_URL = "https://ru.investing.com/equities"
+URL_SITE = "https://ru.investing.com"
 HEADERS = {"User-Agent": UserAgent().random}
 
-async def main():
+async def ALL():
     async with aiohttp.ClientSession() as session:
-         async with session.get(BASE_URL, headers=HEADERS) as response:
+        async with session.get(BASE_URL, headers=HEADERS) as response:
             r = await aiohttp.StreamReader.read(response.content)
             soup = BS(r, 'html.parser')
-            
-            items = soup.find("div", {"class": "text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]"}).text
-            print(items)
+            links = soup.find_all("a", {"class": "overflow-hidden text-ellipsis whitespace-nowrap font-semibold text-[#181C21] hover:text-[#1256A0]"})
+
+            for link in links:
+                URL_EQUITIES = f"{URL_SITE}/{link.get('href')}"
+
+                async with session.get(URL_EQUITIES, headers=HEADERS) as response:
+                    r = await aiohttp.StreamReader.read(response.content)
+                    soup = BS(r, 'html.parser')
+
+                    price = soup.find("div", {"class": "text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]"})
+
+                    if price is not None:
+                        print(f'equities: UNDEFINED\t| price: {price.text} ₽\t| links: {URL_EQUITIES}')
+                    else:
+                        print(f'Error: Price not found for {URL_EQUITIES}')
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(ALL())
