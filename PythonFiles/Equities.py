6dd79b679eea8ebb2b3405edc50c60cffc61bbe9
Undefined <Names2007@mail.ru>
2024-03-30 21:32:40 +0300

    none

--- PythonFiles/Equities.py
+++ PythonFiles/Equities.py
@@ -13,26 +13,26 @@ async def IndexMOEX():
     async with aiohttp.ClientSession() as session:
         async with session.get(BASE_URL, headers=HEADERS) as response:
             r = await aiohttp.StreamReader.read(response.content)
-            variableSITE = BS(r, 'html.parser')
-            links = variableSITE.find_all("a", {"class": "overflow-hidden text-ellipsis whitespace-nowrap font-semibold text-[#181C21] hover:text-[#1256A0]"})
+            soup = BS(r, 'html.parser')
+            links = soup.find_all("a", {"class": "overflow-hidden text-ellipsis whitespace-nowrap font-semibold text-[#181C21] hover:text-[#1256A0]"})
 
             for link in links:
                 URL_EQUITIES = f"{URL_SITE}/{link.get('href')}"
 
                 async with session.get(URL_EQUITIES, headers=HEADERS) as response:
                     r = await aiohttp.StreamReader.read(response.content)
-                    variableSITE = BS(r, 'html.parser')
+                    soup = BS(r, 'html.parser')
 
-                    price = variableSITE.find("div", {"class": "text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]"}).text
-                    title = variableSITE.find("h1", {"class": "mb-2.5 text-left text-xl font-bold leading-7 text-[#232526] md:mb-2 md:text-3xl md:leading-8 rtl:soft-ltr"}).text
+                    price = soup.find("div", {"class": "text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]"}).text
+                    title = soup.find("h1", {"class": "mb-2.5 text-left text-xl font-bold leading-7 text-[#232526] md:mb-2 md:text-3xl md:leading-8 rtl:soft-ltr"}).text
                     
                     print(f'price: {price} ₽ \t | equities: {title}')
 
 
-def main():
+def mainEquities():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(IndexMOEX())
 
 if __name__ == '__main__':
-    main()
+    mainEquities()
     
--- PythonFiles/ProjectEngine.py
+++ PythonFiles/ProjectEngine.py
@@ -1,7 +1,7 @@
 import Equities
 
 if __name__ == '__main__':
-    Equities.main()
+    Equities.mainEquities()
 
 
 # Добавить гены для нейронки. Сделать их как подключаемыми файлами
