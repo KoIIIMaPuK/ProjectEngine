34f27ae0336727ff7f983cf425c90d3ae5bccf46
Undefined <Names2007@mail.ru>
2024-04-01 22:05:31 +0300

    add class on cpp file for create my hash algorithm

---
+++ ProjectEngine/__HF1.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class __HF1
+{
+	std::string initialText;
+	std::string convertedText;
+
+	unsigned __int16 key;
+	unsigned __int32 memory;
+};
